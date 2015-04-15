6cb5adaaebbe2594fb5a5d3c97fa6e1bf02ad71d
b4456609 <b4456609@gmail.com>
2015-04-15 17:55:46 +0800

    finish download

--- src/ntou/easylearn/servlet/FileDownloadManerger.java
+++ src/ntou/easylearn/servlet/FileDownloadManerger.java
@@ -4,8 +4,10 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Base64;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -67,6 +69,59 @@ public class FileDownloadManerger extends HttpServlet {
 			e.printStackTrace();
 		}
 		return bytes.toString();
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		String packId = request.getParameter("pack_id");
+		String versionId = request.getParameter("version_id");
+		String filename = request.getParameter("filename");
+		
+		// reads input file from an absolute path
+		String path = "D:" + File.separator + "easylearn" + File.separator +  packId + File.separator + versionId + File.separator
+				+ filename;
+		//String path = "D:/hello/hello2/hi.jpg";
+        File downloadFile = new File(path);
+        FileInputStream inStream = new FileInputStream(downloadFile);
+         
+        // if you want to use a relative path to context root:
+        String relativePath = getServletContext().getRealPath("");
+        System.out.println("relativePath = " + relativePath);
+         
+        // obtains ServletContext
+        ServletContext context = getServletContext();
+         
+        // gets MIME type of the file
+        String mimeType = context.getMimeType(path);
+        if (mimeType == null) {        
+            // set to binary type if MIME mapping not found
+            mimeType = "application/octet-stream";
+        }
+        System.out.println("MIME type: " + mimeType);
+         
+        // modifies response
+        response.setContentType(mimeType);
+        response.setContentLength((int) downloadFile.length());
+         
+        // forces download
+        String headerKey = "Content-Disposition";
+        String headerValue = String.format("attachment; filename=\"%s\"", downloadFile.getName());
+        response.setHeader(headerKey, headerValue);
+         
+        // obtains response's output stream
+        OutputStream outStream = response.getOutputStream();
+         
+        byte[] buffer = new byte[4096];
+        int bytesRead = -1;
+         
+        while ((bytesRead = inStream.read(buffer)) != -1) {
+            outStream.write(buffer, 0, bytesRead);
+        }
+         
+        inStream.close();
+        outStream.close(); 
 	}
 
 }
